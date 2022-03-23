fbe4b0a5aa3c44b24df46dad917d4874d8f3b663
Lucas Constantino <lucasconstantino0102@gmail.com>
2022-03-23 15:29:50 -0300

    Adicionando adm

---
+++ src/SistemaInterno.kt
@@ -0,0 +1,9 @@
+class SistemaInterno {
+    fun entra(admin: FuncionarioAdmin, senha: Int){
+        if(admin.autentica(senha)){
+            println("Bem vindo ao ByteBank!")
+        } else {
+            println("Falha na autenticação.")
+        }
+    }
+}
